4ce0adf8ac4b54f672f5695a1b2d9730e8a9b0e1
AmandineDubreuil <amandine.dubreuil@hotmail.com>
2023-05-15 12:28:54 +0200

    suppression d un commentaire

--- adminEpiVeto/index.php
+++ adminEpiVeto/index.php
@@ -12,16 +12,7 @@ $bandeau = getActualiteById($idActualite)['bandeau'];
 $carouselUn =getActualiteById($idActualite)['carousel_un'];
 $carouselDeux =getActualiteById($idActualite)['carousel_deux'];
 
-/*
-à remettre dans IndexView
-<section>
-<a href="./utilisateurs/index.php">
-    <h3>Gestion des utilisateurs</h3>
-</a>
-<p>Permet l'ajout, la modification ou la suppression d'un compte utilisateur.</p>
 
-</section>
-*/
 
 
 require '../view/adminEpiVeto/indexView.php';
